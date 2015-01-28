46ec31443b68ed9da22c27d10633b4d5b957228c
Kartik K. Agaram <vc@akkartik.com>
2015-01-28 02:02:00 -0800

    657 - progress with a better color library
    (flush-output) does the trick as suggested in
    https://github.com/hopkinsr/terminal-color/issues/1

--- x.rkt
+++ x.rkt
@@ -3,12 +3,8 @@
 (open-charterm)
 (charterm-clear-screen)
 (charterm-cursor 5 5)
-(displayln-color "Hello" #:fg 'green)  ; works
-(charterm-cursor 25 5)
-(displayln-color " Hello" #:fg 'green)  ; works
-;? ;? (charterm-cursor 1 6) ;? 2
-;? ;? (display-color "Hello" #:fg 'green)  ; err: cursor moves to start of line ;? 1
-;? (charterm-newline)  ; doesn't work after display; somehow runs before it ;? 1
-;? ;? (charterm-clear-line) ;? 3
-;? (displayln-color "World" #:fg 'green) ;? 1
+(display-color "Hello" #:fg 'green)
+(flush-output)
+(charterm-newline)
+(display-color "Hello" #:fg 'green)
 (close-charterm)
