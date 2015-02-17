9e528c1bc276b029e25c9cba7260a9fa75bad12c
Kartik K. Agaram <vc@akkartik.com>
2015-02-17 12:28:45 -0800

    770

--- cpp/002main.test.cc
+++ cpp/002main.test.cc
@@ -1,7 +1,6 @@
 void test_parse() {
   compile("recipe main [\n"
           "  1:integer <- copy 23:literal\n"
-//?           "  1:integer, 2:integer <- copy 23:literal\n"
           "]\n");
   CHECK(Recipe_number.find("main") != Recipe_number.end());
   recipe r = Recipe[Recipe_number["main"]];
@@ -19,6 +18,32 @@ void test_parse() {
   CHECK_EQ(i->products[0].types.size(), 1);
   CHECK_EQ(i->products[0].types[0], Type_number["integer"]);
   CHECK_EQ(i->products[0].properties.size(), 0);
+}
+
+void test_parse2() {
+  compile("recipe main [\n"
+          "  1:integer, 2:integer <- copy 23:literal\n"
+          "]\n");
+  CHECK(Recipe_number.find("main") != Recipe_number.end());
+  recipe r = Recipe[Recipe_number["main"]];
+  vector<instruction>::iterator i = r.step.begin();
+  CHECK_EQ(i->is_label, false);
+  CHECK_EQ(i->label, "");
+  CHECK_EQ(i->operation, Recipe_number["copy"]);
+  CHECK_EQ(i->ingredients.size(), 1);
+  CHECK_EQ(i->ingredients[0].name, string("23"));
+  CHECK_EQ(i->ingredients[0].types.size(), 1);
+  CHECK_EQ(i->ingredients[0].types[0], Type_number["literal"]);
+  CHECK_EQ(i->ingredients[0].properties.size(), 0);
+  CHECK_EQ(i->products.size(), 2);
+  CHECK_EQ(i->products[0].name, string("1"));
+  CHECK_EQ(i->products[0].types.size(), 1);
+  CHECK_EQ(i->products[0].types[0], Type_number["integer"]);
+  CHECK_EQ(i->products[0].properties.size(), 0);
+  CHECK_EQ(i->products[1].name, string("2"));
+  CHECK_EQ(i->products[1].types.size(), 1);
+  CHECK_EQ(i->products[1].types[0], Type_number["integer"]);
+  CHECK_EQ(i->products[1].properties.size(), 0);
 }
 
 void test_literal() {
