5963dacdb07100af6662a5b0366b3fca18e4c2fb
Kartik K. Agaram <vc@akkartik.com>
2015-02-16 16:57:47 -0800

    767

--- cpp/002main.test.cc
+++ cpp/002main.test.cc
@@ -1,3 +1,10 @@
+void test_compile() {
+  compile("recipe main [\n"
+          "  1:integer <- copy 23:literal\n"
+          "]\n");
+  CHECK(Recipe_number.find("main") != Recipe_number.end());
+}
+
 void test_literal() {
   compile("recipe main [\n"
           "  1:integer <- copy 23:literal\n"
