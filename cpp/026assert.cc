2b99af95390af34e7bb13f7be4f212494dd60dda
Kartik K. Agaram <vc@akkartik.com>
2015-04-28 14:25:07 -0700

    1213

---
+++ cpp/026assert.cc
@@ -0,0 +1,20 @@
+:(scenario assert)
+% Hide_warnings = true;
+recipe main [
+  assert 0:literal, [this is an assert in mu]
+]
++warn: this is an assert in mu
+
+:(before "End Primitive Recipe Declarations")
+ASSERT,
+:(before "End Primitive Recipe Numbers")
+Recipe_number["assert"] = ASSERT;
+:(before "End Primitive Recipe Implementations")
+case ASSERT: {
+  trace("run") << "ingredient 0 is " << current_instruction().ingredients[0].name;
+  vector<int> arg0 = read_memory(current_instruction().ingredients[0]);
+  assert(arg0.size() == 1);
+  if (arg0[0] == 0)
+    raise << current_instruction().ingredients[1].name << '\n';
+  break;
+}
