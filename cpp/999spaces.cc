8a7ad05a804aa4f39d5919a581aba552b7c4f011
Kartik K. Agaram <vc@akkartik.com>
2015-05-04 12:19:53 -0700

    1253 - new notion of 'predefined globals in scenarios'

--- cpp/999spaces.cc
+++ cpp/999spaces.cc
@@ -1,11 +1,14 @@
 //: Since different layers all carve out different parts of various namespaces
 //: (recipes, memory, etc.) for their own use, there's no previous place where
-//: we can lay out the big picture of what uses what. So we'll do that here.
+//: we can lay out the big picture of what uses what. So we'll do that here
+//: and just have to manually remember to update it when we move boundaries
+//: around.
 //:
 //:: Memory
 //:
 //: Location 0 - unused (since it can help uncover bugs)
-//: Locations 1-999 - reserved for tests
+//: Locations 1-899 - reserved for tests
+//: Locations 900-999 - reserved for predefined globals in mu scenarios, like keyboard, screen, etc.
 //: Locations 1000 ('Reserved_for_tests') onward - available to the allocator in chunks of size Initial_memory_per_routine.
 //:
 //:: Recipes
