dba6f0f725557467797785d4af19085c01aca626
Kartik K. Agaram <vc@akkartik.com>
2015-05-07 10:40:45 -0700

    1296 - roll back 1295

--- 026assert.cc
+++ 026assert.cc
@@ -11,7 +11,6 @@ ASSERT,
 Recipe_number["assert"] = ASSERT;
 :(before "End Primitive Recipe Implementations")
 case ASSERT: {
-  assert(!current_instruction().ingredients.empty());
   trace("run") << "ingredient 0 is " << current_instruction().ingredients[0].name;
   vector<long long int> arg0 = read_memory(current_instruction().ingredients[0]);
   assert(arg0.size() == 1);
