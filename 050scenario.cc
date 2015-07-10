44221b2e6785d062205119130bc4f69699d0603b
Kartik K. Agaram <vc@akkartik.com>
2015-07-10 15:52:38 -0700

    1751 - sluggishness fixed
    Ah, I was indeed double-rendering, but somehow it was still hard to see
    the problem past that preliminary diagnosis.
    
    Still two failing tests to fix.

--- 050scenario.cc
+++ 050scenario.cc
@@ -131,7 +131,7 @@ const scenario* Current_scenario = NULL;
 void run_mu_scenario(const scenario& s) {
   Current_scenario = &s;
   bool not_already_inside_test = !Trace_stream;
-//?   cerr << s.name << '\n'; //? 9
+//?   cerr << s.name << '\n'; //? 10
   if (not_already_inside_test) {
     Trace_file = s.name;
     Trace_stream = new trace_stream;
