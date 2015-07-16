c8d8cb836baf44f3996c135ca24edc19e1664ff4
Kartik K. Agaram <vc@akkartik.com>
2015-07-15 22:08:21 -0700

    1793 - rudimentary sandboxing for scenarios in the environment

--- 050scenario.cc
+++ 050scenario.cc
@@ -131,7 +131,7 @@ const scenario* Current_scenario = NULL;
 void run_mu_scenario(const scenario& s) {
   Current_scenario = &s;
   bool not_already_inside_test = !Trace_stream;
-//?   cerr << s.name << '\n'; //? 11
+//?   cerr << s.name << '\n'; //? 12
   if (not_already_inside_test) {
     Trace_file = s.name;
     Trace_stream = new trace_stream;
