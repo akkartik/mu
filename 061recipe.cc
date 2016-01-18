af085cf654eb499c1756509dea1d74f93ecbef45
Kartik K. Agaram <vc@akkartik.com>
2016-01-18 09:13:02 -0800

    2563 - bring back 'call'
    Still need to type-check it, though.

--- 061recipe.cc
+++ 061recipe.cc
@@ -2,6 +2,29 @@
 //: also like to make the recipe a variable, pass recipes to "higher-order"
 //: recipes, return recipes from recipes and so on.
 
+:(scenario call_literal_recipe)
+recipe main [
+  1:number <- call f, 34
+]
+recipe f x:number -> y:number [
+  local-scope
+  load-ingredients
+  y <- copy x
+]
++mem: storing 34 in location 1
+
+:(scenario call_variable)
+recipe main [
+  {1: (recipe number -> number)} <- copy f
+  2:number <- call {1: (recipe number -> number)}, 34
+]
+recipe f x:number -> y:number [
+  local-scope
+  load-ingredients
+  y <- copy x
+]
++mem: storing 34 in location 2
+
 :(before "End Mu Types Initialization")
 put(Type_ordinal, "recipe-literal", 0);
 // 'recipe' variables can store recipe-literal
@@ -14,6 +37,37 @@ if (!x.properties.at(0).second && contains_key(Recipe_ordinal, x.name)) {
   x.type = new type_tree(get(Type_ordinal, "recipe-literal"));
   x.set_value(get(Recipe_ordinal, x.name));
   return true;
+}
+
+:(before "End Primitive Recipe Declarations")
+CALL,
+:(before "End Primitive Recipe Numbers")
+put(Recipe_ordinal, "call", CALL);
+:(before "End Primitive Recipe Checks")
+case CALL: {
+  if (inst.ingredients.empty()) {
+    raise_error << maybe(get(Recipe, r).name) << "'call' requires at least one ingredient (the recipe to call)\n" << end();
+    break;
+  }
+  if (!is_mu_recipe(inst.ingredients.at(0))) {
+    raise_error << maybe(get(Recipe, r).name) << "first ingredient of 'call' should be a recipe, but got " << inst.ingredients.at(0).original_string << '\n' << end();
+    break;
+  }
+  break;
+}
+:(before "End Primitive Recipe Implementations")
+case CALL: {
+  // Begin Call
+  if (Trace_stream) {
+    ++Trace_stream->callstack_depth;
+    trace("trace") << "indirect 'call': incrementing callstack depth to " << Trace_stream->callstack_depth << end();
+    assert(Trace_stream->callstack_depth < 9000);  // 9998-101 plus cushion
+  }
+  const instruction& caller_instruction = current_instruction();
+  Current_routine->calls.push_front(call(ingredients.at(0).at(0)));
+  ingredients.erase(ingredients.begin());  // drop the callee
+  finish_call_housekeeping(caller_instruction, ingredients);
+  continue;
 }
 
 :(code)
