d47f3a82786c7d3abdb1001c2562780d0e1fab2e
Kartik Agaram <vc@akkartik.com>
2018-09-21 22:25:00 -0700

    4584 - discrepancy between SubX and native x86
    One of the more painful things I had to debug with machine code. Tricks
    I used can be seen in ex10.subx:
    - printing argv[1] in various places
    - printing a single 'X' in various places to count how many times we get
      to different instructions
    - exiting with the current value of EAX in various places
    
    I repeatedly went down the wrong trail in several ways:
    - forgetting that the problem lay in native runs, and accidentally switching
      to subx runs during debugging.
    - forgetting to pass commandline args, because ex10 doesn't check its argv
    - writing the wrong comment for an instruction, and then miscalculating
      the set of registers that need to be saved.
    - forgetting that syscalls clobber EAX.
    
    Debugging native runs is hard, because you have to write non-trivial code
    to instrument the binary, and instrumentation can itself be buggy.
    
    When we finally tracked it down, I recognized the problem immediately.
    I'd meant to confirm the behavior of opcode 8a against bare metal, and
    then forgot.
    In any case, opcode 8a was inconsistent with 88. Sloppy.

--- subx/014indirect_addressing.cc
+++ subx/014indirect_addressing.cc
@@ -388,16 +388,18 @@ case 0x8b: {  // copy r32 to r/m32
 put(name, "88", "copy r8 (lowermost byte of r32) to r8/m8-at-r32");
 
 :(scenario copy_r8_to_mem_at_r32)
-% Reg[EBX].i = 0xafafafaf;
+% Reg[EBX].i = 0x224488ab;
 % Reg[EAX].i = 0x60;
 == 0x1
 # op  ModR/M  SIB   displacement  immediate
   88  18                                      # copy just the lowermost byte of EBX to the byte at *EAX
 # ModR/M in binary: 00 (indirect mode) 011 (src EBX) 000 (dest EAX)
+== 0x60
+f0 cc bb aa  # 0xf0 with more data in following bytes
 +run: copy lowermost byte of EBX to r8/m8-at-r32
 +run: effective address is 0x60 (EAX)
-+run: storing 0xaf
-% CHECK_EQ(0x000000af, read_mem_u32(0x60));
++run: storing 0xab
+% CHECK_EQ(0xaabbccab, read_mem_u32(0x60));
 
 :(before "End Single-Byte Opcodes")
 case 0x88: {  // copy r/m8 to r8
@@ -417,17 +419,19 @@ case 0x88: {  // copy r/m8 to r8
 put(name, "8a", "copy r8/m8-at-r32 to r8 (lowermost byte of r32)");
 
 :(scenario copy_mem_at_r32_to_r8)
-% Reg[EBX].i = 0xaf;
+% Reg[EBX].i = 0xaabbcc0f;  // one nibble each of lowest byte set to all 0s and all 1s, to maximize value of this test
 % Reg[EAX].i = 0x60;
 == 0x1
 # op  ModR/M  SIB   displacement  immediate
   8a  18                                      # copy just the byte at *EAX to lowermost byte of EBX (clearing remaining bytes)
 # ModR/M in binary: 00 (indirect mode) 011 (dest EBX) 000 (src EAX)
 == 0x60  # data segment
-af ff ff ff  # 0xaf with more data in following bytes
+ab ff ff ff  # 0xab with more data in following bytes
 +run: copy r8/m8-at-r32 to lowermost byte of EBX
 +run: effective address is 0x60 (EAX)
-+run: storing 0xaf
++run: storing 0xab
+# remaining bytes of EBX are *not* cleared
++run: EBX now contains 0xaabbccab
 
 :(before "End Single-Byte Opcodes")
 case 0x8a: {  // copy r/m8 to r8
@@ -436,8 +440,9 @@ case 0x8a: {  // copy r/m8 to r8
   trace(90, "run") << "copy r8/m8-at-r32 to lowermost byte of " << rname(reg1) << end();
   // use unsigned to zero-extend 8-bit value to 32 bits
   uint8_t* arg2 = reinterpret_cast<uint8_t*>(effective_address(modrm));
-  Reg[reg1].u = static_cast<uint32_t>(*arg2);
   trace(90, "run") << "storing 0x" << HEXBYTE << NUM(*arg2) << end();
+  *reinterpret_cast<uint8_t*>(&Reg[reg1].u) = *arg2;  // assumes host is little-endian
+  trace(90, "run") << rname(reg1) << " now contains 0x" << HEXWORD << Reg[reg1].u << end();
   break;
 }
 
