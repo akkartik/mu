577281f64b5e6b7e2ef4a78fb6bc44274caedcb0
Kartik Agaram <vc@akkartik.com>
2019-04-06 00:08:32 -0700

    5060

--- subx/010---vm.cc
+++ subx/010---vm.cc
@@ -240,6 +240,7 @@ inline uint32_t* mem_addr_u32(uint32_t addr) {
     }
   }
   if (result == NULL) {
+    if (Trace_file) Trace_file.flush();
     raise << "Tried to access uninitialized memory at address 0x" << HEXWORD << addr << '\n' << end();
     raise << "The entire 4-byte word should be initialized and lie in a single segment.\n" << end();
   }
