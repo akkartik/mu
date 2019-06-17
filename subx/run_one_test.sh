01b6cfe49e6cdf14edc8c610a3aefc80edb8e1ad
Kartik Agaram <vc@akkartik.com>
2019-06-17 00:23:57 -0700

    support `CFLAGS=-g ./run_one_test.sh ...`

--- subx/run_one_test.sh
+++ subx/run_one_test.sh
@@ -16,12 +16,13 @@ fi
 
 envsubst '$TEST_NAME' < run_one_test.subx > /tmp/run_one_test.subx
 
+set -e
 if [[ $1 == */* ]]
 then
-  subx --debug translate [0-9]*.subx apps/subx-common.subx $1 /tmp/run_one_test.subx -o /tmp/a.elf
+  CFLAGS=$CFLAGS subx --debug translate [0-9]*.subx apps/subx-common.subx $1 /tmp/run_one_test.subx -o /tmp/a.elf
 else
   # don't mention files twice
-  subx --debug translate [0-9]*.subx apps/subx-common.subx    /tmp/run_one_test.subx -o /tmp/a.elf
+  CFLAGS=$CFLAGS subx --debug translate [0-9]*.subx apps/subx-common.subx    /tmp/run_one_test.subx -o /tmp/a.elf
 fi
 
 subx --debug --trace run /tmp/a.elf
