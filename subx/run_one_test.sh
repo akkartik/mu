85cc81a5fb7a7faa7eb40a141d9ed7e840b3f24a
Kartik Agaram <vc@akkartik.com>
2019-07-03 23:39:51 -0700

    .
    Revert a bad merge.

--- subx/run_one_test.sh
+++ subx/run_one_test.sh
@@ -4,9 +4,10 @@
 
 if [[ $2 == 'test-'* ]]
 then
-  export TEST_NAME=$2
-  echo $TEST_NAME > /tmp/last_test_run
-elif [[ -e /tmp/last_test_run ]]
+  TEST_NAME=$2 envsubst '$TEST_NAME' < run_one_test.subx > /tmp/run_one_test.subx
+  FILES=$(ls [0-9]*.subx apps/subx-common.subx $1 |sort |uniq)
+  echo $FILES > /tmp/last_run_files
+elif [[ -e /tmp/last_run_files ]]
 then
   FILES=`cat /tmp/last_run_files`
 else
@@ -15,12 +16,7 @@ else
 fi
 
 set -e
-if [[ $1 == */* ]]
-then
-  CFLAGS=$CFLAGS ./subx --debug translate [0-9]*.subx  apps/subx-common.subx $1 /tmp/run_one_test.subx -o /tmp/a.elf
-else
-  # don't mention files twice
-  CFLAGS=$CFLAGS ./subx --debug translate [0-9]*.subx apps/subx-common.subx    /tmp/run_one_test.subx -o /tmp/a.elf
-fi
+                                      # turn newlines into spaces
+CFLAGS=$CFLAGS ./subx --debug translate $(echo $FILES) /tmp/run_one_test.subx -o /tmp/a.elf
 
 ./subx --debug --trace run /tmp/a.elf
